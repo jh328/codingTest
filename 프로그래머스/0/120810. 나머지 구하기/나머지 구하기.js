8cbb7011c4cd8744fe981a6e4b2bbdc5de511702
jh328 <158754584+jh328@users.noreply.github.com>
2025-07-16 00:19:11 +0900

    [level 0] Title: 나머지 구하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120810. 나머지 구하기/나머지 구하기.js
+++ 프로그래머스/0/120810. 나머지 구하기/나머지 구하기.js
@@ -1,4 +1,9 @@
 function solution(num1, num2) {
-    let answer = num1 % num2
-    return answer;
-}
+    if((num1 %  num2) === 1){
+        return 1;
+    }else if((num1 % num2) === 0){
+        return 0;
+    }else {
+        return num1 % num2;
+    }
+}   
