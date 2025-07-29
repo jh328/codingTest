163ce6ef988402f5e3aa7f25538498a5c66a9fb6
jh328 <158754584+jh328@users.noreply.github.com>
2025-07-29 15:12:06 +0900

    [level 0] Title: 배열 자르기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120833. 배열 자르기/배열 자르기.js
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.js
@@ -1,5 +1,8 @@
 function solution(numbers, num1, num2) {
+    let result = [];
     
-    return numbers.slice(num1,num2+1)
-
+    for (let i = num1; i<=num2; i++){
+        result.push(numbers[i])
+    }
+    return result
 }
