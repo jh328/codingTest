d9b11028e303f3033f59a85962f30bea779db3b1
jh328 <158754584+jh328@users.noreply.github.com>
2025-08-07 18:17:01 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.06 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -1,16 +1,4 @@
-// sides 매개변수는 배열값
-// 배열안에 있는 인덱스를 꺼내서, 제일 큰 정수와, 나머지 두개의 정수를 합쳤을 때, 
-// big > sum = 2;
-// big < sum = 1;
-// let sides = [1,2,3]
 function solution(sides) {
-    let maxNum = Math.max.apply(null,sides)
-    let totalNum = sides[0] + sides[1] + sides[2];
-    let result = totalNum - maxNum;
-    //     3(271)        6(493)     (222)     3 
-    if(maxNum <result){
-        return 1;
-    }else{
-        return 2;
-    }
+    let copySides = [...sides].sort((a,b) => a-b);
+    return copySides[0] + copySides[1] > copySides[2] ? 1 : 2
 }
