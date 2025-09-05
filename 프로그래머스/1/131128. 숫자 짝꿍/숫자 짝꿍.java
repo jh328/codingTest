f83175bef89f79dd78f61508688909c61aec77bd
jh328 <158754584+jh328@users.noreply.github.com>
2025-09-05 16:43:59 +0900

    [level 1] Title: 숫자 짝꿍, Time: 89.85 ms, Memory: 157 MB -BaekjoonHub

---
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -0,0 +1,37 @@
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+class Solution {
+    public String solution(String X, String Y) {
+        int[] countX = new int[10];
+        int[] countY = new int[10];
+
+        for (char c : X.toCharArray()) {
+            countX[c - '0']++;
+        }
+        for (char c : Y.toCharArray()) {
+            countY[c - '0']++;
+        }
+
+        StringBuilder answerBuilder = new StringBuilder();
+
+        for (int i = 9; i >= 0; i--) {
+            int commonCount = Math.min(countX[i], countY[i]);
+            for (int j = 0; j < commonCount; j++) {
+                answerBuilder.append(i);
+            }
+        }
+
+        String answer = answerBuilder.toString();
+
+        if (answer.isEmpty()) {
+            return "-1";
+        }
+        
+        if (answer.charAt(0) == '0') {
+            return "0";
+        }
+
+        return answer;
+    }
+}
