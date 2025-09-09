f02bc925b88654baf2d901cc4c0db8ab69d17091
jh328 <158754584+jh328@users.noreply.github.com>
2025-09-10 00:21:46 +0900

    [level 0] Title: 문자열로 변환, Time: 0.78 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/0/181845. 문자열로 변환/문자열로 변환.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        for(int i = 0; i < n; i++){
+            String str = Integer.toString(n);
+            answer = str;
+        }
+        return answer;
+    }
+}
