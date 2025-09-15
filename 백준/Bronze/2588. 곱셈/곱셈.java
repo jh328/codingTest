8d49eada2fc2c5e97f7a47f181684d480ad11739
jh328 <158754584+jh328@users.noreply.github.com>
2025-09-15 19:42:23 +0900

    [Bronze III] Title: 곱셈, Time: 168 ms, Memory: 17604 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int origin = b;
+
+        while (b > 0) {
+            int init = b % 10;
+
+            System.out.println(a * init);
+
+            b = b / 10;
+        }
+
+        System.out.println(a * origin);
+
+        sc.close();
+    }
+}
